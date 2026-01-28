5a9a2be6d4aba45286f26341334cbda2d6c4d114
Hugues Poncet <hugues.poncet@testwe.eu>
2026-01-28 15:13:34 +0100

    feat: update icons to png

--- src/resources.ts
+++ src/resources.ts
@@ -1,6 +1,32 @@
 import path from "path";
 import { ExtensionContext, Uri } from "vscode";
 
+export enum Icons {
+  issue = "issue",
+  issueDark = "issue-dark",
+  startWork = "startWork",
+  startWorkDark = "startWork-dark",
+  treeIssue = "treeIssue",
+  treeIssueDark = "treeIssue-dark",
+  linear = "linear",
+  linearDark = "linear-dark",
+  backlog = "backlog",
+  canceled = "canceled",
+  completed = "completed",
+  unstarted = "unstarted",
+  started0 = "started0",
+  started1 = "started1",
+  started2 = "started2",
+  started3 = "started3",
+  started4 = "started4",
+  started5 = "started5",
+  started6 = "started6",
+  started7 = "started7",
+  started8 = "started8",
+  started9 = "started9",
+  triage = "triage",
+}
+
 export class Resources {
   public icons = new Map<string, Uri>();
 
@@ -9,83 +35,107 @@ export class Resources {
     const statuesPath = path.join(imagesPath, "statues");
 
     this.icons.set(
-      "issue",
-      Uri.file(context.asAbsolutePath(path.join(imagesPath, "issue.svg")))
+      Icons.issue,
+      Uri.file(context.asAbsolutePath(path.join(imagesPath, "issue.png"))),
     );
     this.icons.set(
-      "startWork",
-      Uri.file(context.asAbsolutePath(path.join(imagesPath, "start-work.svg")))
+      Icons.issue,
+      Uri.file(context.asAbsolutePath(path.join(imagesPath, "issue_dark.png"))),
     );
     this.icons.set(
-      "treeIssue",
-      Uri.file(context.asAbsolutePath(path.join(imagesPath, "tree-issue.svg")))
+      Icons.startWork,
+      Uri.file(
+        context.asAbsolutePath(path.join(imagesPath, "start-work_dark.png")),
+      ),
     );
     this.icons.set(
-      "linear",
-      Uri.file(context.asAbsolutePath(path.join(imagesPath, "linear.svg")))
+      Icons.treeIssue,
+      Uri.file(
+        context.asAbsolutePath(path.join(imagesPath, "tree-issue_dark.png")),
+      ),
+    );
+    this.icons.set(
+      Icons.linear,
+      Uri.file(
+        context.asAbsolutePath(path.join(imagesPath, "linear_dark.png")),
+      ),
     );
 
     this.icons.set(
-      "backlog",
-      Uri.file(context.asAbsolutePath(path.join(statuesPath, "backlog.svg")))
+      Icons.canceled,
+      Uri.file(context.asAbsolutePath(path.join(statuesPath, "canceled.png"))),
     );
     this.icons.set(
-      "canceled",
-      Uri.file(context.asAbsolutePath(path.join(statuesPath, "canceled.svg")))
+      Icons.completed,
+      Uri.file(context.asAbsolutePath(path.join(statuesPath, "completed.png"))),
     );
     this.icons.set(
-      "completed",
-      Uri.file(context.asAbsolutePath(path.join(statuesPath, "completed.svg")))
-    );
-    this.icons.set(
-      "unstarted",
-      Uri.file(context.asAbsolutePath(path.join(statuesPath, "unstarted.svg")))
+      Icons.unstarted,
+      Uri.file(context.asAbsolutePath(path.join(statuesPath, "unstarted.png"))),
     );
 
     this.icons.set(
-      "started0",
-      Uri.file(context.asAbsolutePath(path.join(statuesPath, "started0.svg")))
+      Icons.started0,
+      Uri.file(context.asAbsolutePath(path.join(statuesPath, "started0.png"))),
     );
     this.icons.set(
-      "started1",
-      Uri.file(context.asAbsolutePath(path.join(statuesPath, "started1.svg")))
+      Icons.started1,
+      Uri.file(context.asAbsolutePath(path.join(statuesPath, "started1.png"))),
     );
     this.icons.set(
-      "started2",
-      Uri.file(context.asAbsolutePath(path.join(statuesPath, "started2.svg")))
+      Icons.started2,
+      Uri.file(context.asAbsolutePath(path.join(statuesPath, "started2.png"))),
     );
     this.icons.set(
-      "started3",
-      Uri.file(context.asAbsolutePath(path.join(statuesPath, "started3.svg")))
+      Icons.started3,
+      Uri.file(context.asAbsolutePath(path.join(statuesPath, "started3.png"))),
     );
     this.icons.set(
-      "started4",
-      Uri.file(context.asAbsolutePath(path.join(statuesPath, "started4.svg")))
+      Icons.started4,
+      Uri.file(context.asAbsolutePath(path.join(statuesPath, "started4.png"))),
     );
     this.icons.set(
-      "started5",
-      Uri.file(context.asAbsolutePath(path.join(statuesPath, "started5.svg")))
+      Icons.started5,
+      Uri.file(context.asAbsolutePath(path.join(statuesPath, "started5.png"))),
     );
     this.icons.set(
-      "started6",
-      Uri.file(context.asAbsolutePath(path.join(statuesPath, "started6.svg")))
+      Icons.started6,
+      Uri.file(context.asAbsolutePath(path.join(statuesPath, "started6.png"))),
     );
     this.icons.set(
-      "started7",
-      Uri.file(context.asAbsolutePath(path.join(statuesPath, "started7.svg")))
+      Icons.started7,
+      Uri.file(context.asAbsolutePath(path.join(statuesPath, "started7.png"))),
     );
     this.icons.set(
-      "started8",
-      Uri.file(context.asAbsolutePath(path.join(statuesPath, "started8.svg")))
+      Icons.started8,
+      Uri.file(context.asAbsolutePath(path.join(statuesPath, "started8.png"))),
     );
     this.icons.set(
-      "started9",
-      Uri.file(context.asAbsolutePath(path.join(statuesPath, "started9.svg")))
+      Icons.started9,
+      Uri.file(context.asAbsolutePath(path.join(statuesPath, "started9.png"))),
     );
 
     this.icons.set(
-      "triage",
-      Uri.file(context.asAbsolutePath(path.join(statuesPath, "triage.svg")))
+      Icons.triage,
+      Uri.file(context.asAbsolutePath(path.join(statuesPath, "triage.png"))),
     );
+
+    // Light theme icons
+    // this.icons.set(
+    //   Icons.startWork,
+    //   Uri.file(context.asAbsolutePath(path.join(imagesPath, "start-work.png"))),
+    // );
+    // this.icons.set(
+    //   Icons.treeIssue,
+    //   Uri.file(context.asAbsolutePath(path.join(imagesPath, "tree-issue.png"))),
+    // );
+    // this.icons.set(
+    //   Icons.linear,
+    //   Uri.file(context.asAbsolutePath(path.join(imagesPath, "linear.png"))),
+    // );
+    // this.icons.set(
+    //   Icons.backlog,
+    //   Uri.file(context.asAbsolutePath(path.join(statuesPath, "backlog.png"))),
+    // );
   }
 }
